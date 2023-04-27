8a655d8014a082462a681c2ea94c36b5812b2fe5
Tobias Bertera <tobiasbertera@gmail.com>
2023-04-27 19:20:22 -0300

    working on the counter. Safe place for now, changing the return values for the console.log()

--- rock-paper.js
+++ rock-paper.js
@@ -14,19 +14,54 @@ let machineChoice = getMachineChoice();
 console.log(machineChoice);
 
 function play(userChoice, machineChoice) {
-  if (userChoice === machineChoice){
-    return "Its a tie!";
-  } else if (userChoice === "rock" && machineChoice === "scissors"){
-    return "You win! rock beats scissors";
-  } else if (userChoice === "paper" && machineChoice === "rock"){
-    return "You win! paper beats rock";
-  } else if (userChoice === "scissors" && machineChoice === "paper"){
-    return "You win! scissors beat paper";
-  } else {
-    return `You lose! ${machineChoice} beats ${userChoice}`;
-  }
-} 
-console.log(play(userChoice, machineChoice)); 
 
+let userWin = 0;
+let machineWin = 0;
+
+  if (userChoice === machineChoice){
+   console.log("Its a tie!"); 
+   //return "Its a tie!";  
+    
+  } else if (userChoice === "rock" && machineChoice === "scissors"){
+    userWin = userWin++
+    console.log("You win! rock beats scissors");
+   //return "You win! rock beats scissors";
+
+  } else if (userChoice === "paper" && machineChoice === "rock"){
+    userWin = userWin++
+    console.log("You win! paper beats rock");
+    //return "You win! paper beats rock";
+
+  } else if (userChoice === "scissors" && machineChoice === "paper"){
+    userWin = userWin++
+    console.log("You win! scissors beat paper");
+    //return "You win! scissors beat paper";
+
+  } else {
+    machineWin = machineWin++
+    console.log(`You lose! ${machineChoice} beats ${userChoice}`);
+   // return `You lose! ${machineChoice} beats ${userChoice}`;
+
+  }
+ 
+  }
   
+  console.log(play(userChoice, machineChoice)); 
+   function contador(userWin, machineWin) {
+      if (userWin === 1) {
+        return userWin;
+      } else {
+        return machineWin;
+      }
+} 
+
+
+function game() {
+    for (i=1; i<= 5; i++) {
+        play(userChoice, machineChoice);
+        
+        console.log(play());
+    }
+    
+}
 
