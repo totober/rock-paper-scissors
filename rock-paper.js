21b1945bf76fded50856cdf39fc8651404fec9ac
Tobias Bertera <tobiasbertera@gmail.com>
2023-04-28 10:22:21 -0300

    Ultima parte del ejercicio. Ciclo para jugar 5 partidas y dar un ganador.

--- rock-paper.js
+++ rock-paper.js
@@ -15,53 +15,58 @@ console.log(machineChoice);
 
 function play(userChoice, machineChoice) {
 
-let userWin = 0;
-let machineWin = 0;
+let userWin = 0
+let machineWin = 0
 
-  if (userChoice === machineChoice){
-   console.log("Its a tie!"); 
-   //return "Its a tie!";  
+  if (userChoice === machineChoice){ 
+   return "Its a tie!";  
     
   } else if (userChoice === "rock" && machineChoice === "scissors"){
-    userWin = userWin++
+    
     console.log("You win! rock beats scissors");
    //return "You win! rock beats scissors";
-
+    return userWin = userWin + 1
   } else if (userChoice === "paper" && machineChoice === "rock"){
-    userWin = userWin++
+
     console.log("You win! paper beats rock");
     //return "You win! paper beats rock";
+    return userWin = userWin + 1
 
   } else if (userChoice === "scissors" && machineChoice === "paper"){
-    userWin = userWin++
+    
     console.log("You win! scissors beat paper");
     //return "You win! scissors beat paper";
+    return userWin = userWin + 1
 
   } else {
-    machineWin = machineWin++
+    
     console.log(`You lose! ${machineChoice} beats ${userChoice}`);
-   // return `You lose! ${machineChoice} beats ${userChoice}`;
+   //return `You lose! ${machineChoice} beats ${userChoice}`;
+   return `Machine total wins: ${machineWin = machineWin + 1} `
 
   }
  
   }
   
-  console.log(play(userChoice, machineChoice)); 
-   function contador(userWin, machineWin) {
-      if (userWin === 1) {
-        return userWin;
-      } else {
-        return machineWin;
-      }
-} 
+  console.log(play(userChoice, machineChoice));
 
 
 function game() {
-    for (i=1; i<= 5; i++) {
-        play(userChoice, machineChoice);
+    for (i=1; i<= 5; i++){
+
+      let userTotal = 0;
+      let machineTotal = 0;
+      let total = `Machine total wins: ${machineTotal} , User total wins: ${userTotal} `;
+
+       play(userChoice, machineChoice); 
+        if (userWin == 1){
+          userTotal++
+        } else {
+          machineTotal++
+        }
         
-        console.log(play());
+        return total
+  
     }
-    
 }
 
