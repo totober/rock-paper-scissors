5186312780094770b10f7b54e526a0128e561ddd
Tobias Bertera <tobiasbertera@gmail.com>
2023-04-28 19:43:02 -0300

    90% terminado. Solo queda reiniciar el juego o el contador

--- rock-paper.js
+++ rock-paper.js
@@ -1,6 +1,14 @@
-let userChoice = prompt("Choose your weapon: rock, paper or scissors").toLowerCase();
-console.log(userChoice);
 
+function getUserChoice () {
+
+ let userValue = prompt("Choose your weapon: rock, paper or scissors").toLowerCase();
+
+ return userValue
+}
+
+let userChoice = getUserChoice();
+
+console.log(userChoice);
 
 function getMachineChoice () {
     let values = ["rock", "paper", "scissors"];
@@ -8,65 +16,75 @@ function getMachineChoice () {
     return randomValue;
 } 
 
-
 let machineChoice = getMachineChoice();
 
 console.log(machineChoice);
 
+  let userCounter = 0;
+  let machineCounter = 0;
+  let finalScore = `User score ${userCounter}, Machine score ${machineCounter}`;
+
 function play(userChoice, machineChoice) {
 
-let userWin = 0
-let machineWin = 0
-
   if (userChoice === machineChoice){ 
-   return "Its a tie!";  
+   
+   return console.log("Its a tie!") ;
     
   } else if (userChoice === "rock" && machineChoice === "scissors"){
     
     console.log("You win! rock beats scissors");
    //return "You win! rock beats scissors";
-    return userWin = userWin + 1
+    return  userCounter += 1
   } else if (userChoice === "paper" && machineChoice === "rock"){
 
     console.log("You win! paper beats rock");
     //return "You win! paper beats rock";
-    return userWin = userWin + 1
+    return  userCounter += 1
 
   } else if (userChoice === "scissors" && machineChoice === "paper"){
     
     console.log("You win! scissors beat paper");
     //return "You win! scissors beat paper";
-    return userWin = userWin + 1
+    return userCounter += 1
 
   } else {
     
     console.log(`You lose! ${machineChoice} beats ${userChoice}`);
    //return `You lose! ${machineChoice} beats ${userChoice}`;
-   return `Machine total wins: ${machineWin = machineWin + 1} `
+   return machineCounter += 1
 
   }
- 
-  }
-  
-  console.log(play(userChoice, machineChoice));
 
+  }
+    
+
+let roundResult = play(userChoice, machineChoice);
+console.log(roundResult);
 
 function game() {
-    for (i=1; i<= 5; i++){
+  play(getUserChoice(),getMachineChoice());
+  console.log(userCounter);
+  console.log(machineCounter);
+  play(getUserChoice(),getMachineChoice());
+  console.log(userCounter);
+  console.log(machineCounter); 
+  play(getUserChoice(),getMachineChoice());
+  console.log(userCounter);
+  console.log(machineCounter);
+  play(getUserChoice(),getMachineChoice());
+  console.log(userCounter);
+  console.log(machineCounter);
+  play(getUserChoice(),getMachineChoice());
+  console.log(userCounter);
+  console.log(machineCounter);
+ 
 
-      let userTotal = 0;
-      let machineTotal = 0;
-      let total = `Machine total wins: ${machineTotal} , User total wins: ${userTotal} `;
-
-       play(userChoice, machineChoice); 
-        if (userWin == 1){
-          userTotal++
-        } else {
-          machineTotal++
-        }
-        
-        return total
-  
-    }
+if (userCounter>machineCounter) {
+  return "Congrats! you win"
+} else if (machineCounter>userCounter) {
+  return "Too bad! you lose"
+} else {
+  return "Wow! its a tie"
 }
 
+}
